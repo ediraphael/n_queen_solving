a1611ad14d0454ee0464f04324aa975a262918d5
ediraphael <raphael.pillie@gmail.com>
2014-05-14 14:26:48 +0200

    Implémentation de la class AbstractResearch

--- src/model/AbstractResearch.java
+++ src/model/AbstractResearch.java
@@ -1,6 +1,30 @@
 package model;
 
+import java.util.ArrayList;
+
 public abstract class AbstractResearch
 {
+	private ArrayList<Chess> solutions;
 
+	public AbstractResearch()
+	{
+		this.solutions = new ArrayList<Chess>();
+	}
+
+	public AbstractResearch(ArrayList<Chess> solutions)
+	{
+		this.solutions = solutions;
+	}
+	
+	public abstract void research();
+
+	public ArrayList<Chess> getSolutions()
+	{
+		return solutions;
+	}
+
+	public void setSolutions(ArrayList<Chess> solutions)
+	{
+		this.solutions = solutions;
+	}
 }
