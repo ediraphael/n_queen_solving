380d81d5ea6d72f462bb109a811754c3c99ea2fc
ediraphael <raphael.pillie@gmail.com>
2014-05-14 10:26:03 +0200

    Début d'implémentation de la class Chess

--- src/model/Chess.java
+++ src/model/Chess.java
@@ -2,5 +2,31 @@ package model;
 
 public class Chess
 {
+	private int[] queenPosition;
+
+	public Chess(int sizeOfChess)
+	{
+		this.queenPosition = new int[sizeOfChess];
+		for (int i = 0; i < queenPosition.length; i++)
+		{
+			queenPosition[i] = 0;
+		}
+	}
+
+	public Chess(int[] queenPosition)
+	{
+		super();
+		this.queenPosition = queenPosition;
+	}
+
+	public int[] getQueenPosition()
+	{
+		return queenPosition;
+	}
+
+	public void setQueenPosition(int[] queenPosition)
+	{
+		this.queenPosition = queenPosition;
+	}
 
 }
